e2ac9f099d9040ac43452b7dcbee1937a39e8a14
linglongxin24 <linglongxin24@163.com>
2016-12-23 14:14:01 +0800

    first commit

---
+++ app/src/main/java/cn/bluemobi/dylan/vrdevelop/MainActivity.java
@@ -0,0 +1,102 @@
+package cn.bluemobi.dylan.vrdevelop;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaEventListener;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * 显示VR360度全景图片的控件
+     */
+    private VrPanoramaView vr_pan_view;
+    /**
+     * 打印的TAG
+     */
+    private final String TAG = "VrPanoramaView";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        load360Image();
+    }
+
+    /**
+     * 加载360度全景图片
+     */
+    private void load360Image() {
+        vr_pan_view = (VrPanoramaView) findViewById(R.id.vr_pan_view);
+        /**获取assets文件夹下的图片**/
+        InputStream open = null;
+        try {
+            open = getAssets().open("andes.jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(open);
+        /**设置加载VR图片的相关设置**/
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+        options.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+        /**设置加载VR图片监听**/
+        vr_pan_view.setEventListener(new VrPanoramaEventListener() {
+            /**
+             * 显示模式改变回调
+             * 1.默认
+             * 2.全屏模式
+             * 3.VR观看模式
+             * @param newDisplayMode 模式
+             */
+            @Override
+            public void onDisplayModeChanged(int newDisplayMode) {
+                super.onDisplayModeChanged(newDisplayMode);
+                Log.d(TAG, "onDisplayModeChanged()->newDisplayMode=" + newDisplayMode);
+            }
+
+            /**
+             * 加载VR图片失败回调
+             * @param errorMessage
+             */
+            @Override
+            public void onLoadError(String errorMessage) {
+                super.onLoadError(errorMessage);
+                Log.d(TAG, "onLoadError()->errorMessage=" + errorMessage);
+            }
+
+            /**
+             * 加载VR图片成功回调
+             */
+            @Override
+            public void onLoadSuccess() {
+                super.onLoadSuccess();
+                Log.d(TAG, "onLoadSuccess->图片加载成功");
+            }
+
+            /**
+             * 点击VR图片回调
+             */
+            @Override
+            public void onClick() {
+                super.onClick();
+                Log.d(TAG, "onClick()");
+            }
+        });
+        /**加载VR图片**/
+        vr_pan_view.loadImageFromBitmap(bitmap, options);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        /**关闭加载VR图片，释放内存**/
+        vr_pan_view.shutdown();
+    }
+}
